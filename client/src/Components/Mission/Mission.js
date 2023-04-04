8ba1335e50e82542cf72e86699d8777af66435e2
faffietheripper <121829394+faffietheripper@users.noreply.github.com>
2023-04-04 08:40:14 +0100

    add content

--- client/src/Components/Mission/Mission.js
+++ client/src/Components/Mission/Mission.js
@@ -13,7 +13,7 @@ export default function Mission() {
             <div class="relative h-64 overflow-hidden sm:h-80 lg:h-full">
               <img
                 alt="Party"
-                src="https://images.unsplash.com/photo-1496843916299-590492c751f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1771&q=80"
+                src="https://lh3.googleusercontent.com/yJ0zPdnvDQ-drxYS1RrR38vIY91ychOHgMrbiXrfYHkAsEMJsfzKDZr1A_jTdEyYtN6fOiUlmaeko44H1kemTN166mioG-7ZYjui-xrXWGGBxjeFqi5YTytGvkMGi0H32L_byaMWvH9c424er2YzXPyuPn63hDawAgHUoDPKmpdDq5lNzTZy_p79CY7d3Sm4TebXy4n7vW8IpO32PjcilMyYNt9sr1l-vTEGqb8MO26c8hKbSt5FAEVqr2KdU52e0HgxKrA1rg9hf4Jc96g2B32YyDjHTq1dAGMJzFNMi-_0mo6-mfPKojui9syswp7ok2KHZWcpzVkakLDS-4k9oI4WdJrEv6tSN-0OnwaMq6mwOaF_BETT8luQTD-Q5LTOMbfNAmPZ1nYG2KHRoXjvifHHq_rSa0aREx2b1rE60udZovak-FXfMVVw27qr-8Lv5NXR7W8ut7e_h5rTUtP3DNTgAcnDvMpWaxVMMDH2ftW8GRSJ-GF04M5ZVBtfvLrj1K0vLKXYyBnHJjqaQhIIaJE1RJQfyoWPs3EDMkXeOz3rzxNMBa0uJ0NWPBJjuG3oi-SRXgKsC1RMC78knjZ2JjuNDbTUiJVxcCdmlavezVszd_4x-RomnP6s8ZdaVpszUkQYmrFtUz859nDeWUgc-H4vmKXdOyUBzZzixcemmR3_KTakqFsNcFrucFDQLjYRQdOlfrFHnaV9o6eilsss_QSpGa-aC3ve3OdLe9v84QTJv3ROzJH7NbDdHEDaDlFJ13rC3SuJlNMQ4Dh3dGsMJ43HbceQrThjfd2asluvT5V2gXxoUn3b98AUS5U26RI2zHSJ0hsCjQQyBMV_dVMsuuMyyxuOCVR8vMkDMQI0JWu6DgoRwYM8pDWXSbya2JfeXuRPP6Bocm391Xy9Up7bskUbzL5_elsiOSCR5aLJ7G9H0KGG=w1080-h717-s-no?authuser=0"
                 class="absolute inset-0 h-full w-full object-cover"
               />
             </div>
