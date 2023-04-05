9816ce9c6aa62bd744e79e6b2db87e02a98ae49d
faffietheripper <121829394+faffietheripper@users.noreply.github.com>
2023-04-05 22:08:58 +0100

    medville changes

---
+++ client/src/Components/MedvilleDesc/MedvilleDesc.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function MedvilleDesc() {
+  return (
+    <div>
+      <section>
+        <div class="mx-auto max-w-screen-xl px-4 py-8 sm:py-12 sm:px-6 lg:py-16 lg:px-8">
+          <div class="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+            <div class="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
+              <img
+                alt="Party"
+                src="https://lh3.googleusercontent.com/JNeO_6owZiF0K25xbuhvXHHvYogl9IUYqMxgrn1WcSJnyAytNZ0LZwiXNqmi1l671pz4_yNvEKZG3vOW0bVMUYLJqOOp9Vn97rFNWbzDEqF2BPTMERZg2hXYsLSJMmbzERpBbnj6JsbXx1KlY3ggQDoGUzHqlRYg8ErKld7odjVEf3gQ9yZ4CCIwiOZXg-mXdSAAdYQFPeW0mb2aY1OqY-WcdzDuv1mNgB-x5ILhSuzBk644Ijl5DZGHeI98NrJiwVdQ_9CCXp1wd2j8w-tNhqn1WJo_DVhSLsFSFq_yclklaZTYvu4kHX8Z9HgbUExOySJRhhBoINyR7JLhfeOpLYNChs7Jw9QB3BZKWJiUicHti8105ACf-Q1WNLMIG6WqTcdi7LrElmyrYwKiuBTdRa2SS8_GeLOrGIhZFzz2vygwAW8WlXPxrNfZvc_OdVVeJVABTMMdbJteW-yQwnFpew6QFHTMsybTvRWCmGrgkyH4-kBV7X_Pazdt5fThV2RdbAPT5uGJ6siTuvc6zP3BbBLtMNYKXzyiGhgEYnPezkbvhBbH-m-5e0r7KSc9MWRFevhNCe6EUw1pWSQxTMyo-eZYs2hVclX_ukm7W-PnNO0Z8ON6z3EQqEezrYCLtK8yvhvGh8K7CIribR36F4liscpkwtnFwebP3oklrkZvwbdx7x_CzHduBfDd0QY0KSHD6GWmzRsFxrdHk6OCBVd_zI1FuHUJ-rGRKF8HpGEfySr9OL8ERkp-GoPZat498aRowEzFW1V2C5wgvuFX6ZdV__5UbyZciTnfV1putlImBvc76JQUGcVeIY7fno0IZaxnIqMNeZPOkAvhPJIKtmaU-71z6-bf71PINf6pZ6GpJi1lhCWaH92njd27ynvTZzU7co5Qzk2frMGOCCPNZOqTZxKNnwCwV9yuVAEZ8Jl5OKvmlM2g=w1080-h1064-s-no?authuser=0"
+                class="absolute inset-0 h-full w-full object-cover"
+              />
+            </div>
+
+            <div class="lg:py-24">
+              <h2 class="text-3xl font-bold sm:text-4xl text-white">Medville Healthcare Online</h2>
+
+              <p class="mt-4 text-gray-600">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut qui hic atque tenetur quis eius quos ea neque sunt, accusantium soluta
+                minus veniam tempora deserunt? Molestiae eius quidem quam repellat.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
--- client/src/Pages/Medville/Medville.js
+++ client/src/Pages/Medville/Medville.js
@@ -1,10 +1,12 @@
 import React from "react";
+import MedvilleDesc from "../../Components/MedvilleDesc/MedvilleDesc";
 import PageUnderCon from "../../Components/PageUnderCon/PageUnderCon";
 
 function Medville() {
   return (
     <div>
       <PageUnderCon />
+      <MedvilleDesc />
     </div>
   );
 }
