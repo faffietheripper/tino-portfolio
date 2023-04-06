715b20ef705f6c24f408dbd6398c51b9d6bd638f
faffietheripper <121829394+faffietheripper@users.noreply.github.com>
2023-04-06 03:19:21 +0100

    resume upload

--- client/src/Components/Skills/Skills.js
+++ client/src/Components/Skills/Skills.js
@@ -15,7 +15,7 @@ export default function Skills() {
               </p>
 
               <a
-                href=" https://drive.google.com/file/d/12FBV5APG9Q0KJu_gDbvOFkFkgmaOUrDS/view?usp=share_link"
+                href="https://resume.io/r/cxMvqHpCd"
                 download
                 class="mt-8 inline-block rounded bg-blue-300 px-12 py-3 text-sm font-medium text-white transition hover:bg-red-400 focus:outline-none focus:ring focus:ring-yellow-400"
               >
