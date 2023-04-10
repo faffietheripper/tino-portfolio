447ce2704f64a935e7c3c4b000d1d3f43350069d
faffietheripper <121829394+faffietheripper@users.noreply.github.com>
2023-04-10 13:34:24 +0100

    Render images

--- client/src/Components/TestimonialsGrid/TestimonialsGrid.js
+++ client/src/Components/TestimonialsGrid/TestimonialsGrid.js
@@ -1,5 +1,7 @@
 import React from "react";
 import SingleTestimonial from "../SingleTestimonial/SingleTestimonial";
+import Image from "../../Images/RuthTest.jpeg";
+import Image2 from "../../Images/TimTest.jpg"
 
 export default function TestimonialsGrid() {
   return (
@@ -15,13 +17,13 @@ export default function TestimonialsGrid() {
           <div class="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 mx-8">
             <SingleTestimonial
               name="Ruth Nyandoro"
-              img="/Users/student/projects/portfolio/client/src/Images/RuthTest.jpeg"
+              img={Image}
               message="I love the website Tino made for Zibah Creations. It is precisely what I needed to get my business noticed on an international level. The website setup is nothing short of perfect and to the point.  It covers all angles necessary for an online business operation and is easy to navigate. It is a job well done."
               role="Founder - Zibah Creations"
             />
             <SingleTestimonial
               name="Tim Smith"
-              img="/Images/TimTest.jpg"
+              img={Image2}
               role="Course Director - Tech Educators"
               message="I had the pleasure of being Tino's instructor on a coding bootcamp, and I cannot say enough positive things about him. Tino is not only highly motivated and eager to learn (which paid dividends in his coding skills) but also incredibly personable and easy to work with. His passion for coding and eagerness to learn new things is truly inspiring. The amount he's gone on to learn in his own time after the course is staggering, and is a testament to his work ethic. It was a pleasure teaching Tino, and I look forward to seeing him excel in his future endeavors."
             />
