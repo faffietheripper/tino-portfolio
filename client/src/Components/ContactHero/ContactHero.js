bd527cc9d853995e7c18994e3e89ae12d4eaacb8
faffietheripper <121829394+faffietheripper@users.noreply.github.com>
2023-04-10 13:57:01 +0100

    Images render

--- client/src/Components/ContactHero/ContactHero.js
+++ client/src/Components/ContactHero/ContactHero.js
@@ -1,11 +1,11 @@
 import React from "react";
-import ContactHero from "../../Images/ContactHero.jpeg";
+import ContactHeroImg from "../../Images/ContactHeroImg.jpeg";
 
 export default function ContactHero() {
   return (
     <div>
       <div class="flex flex-col h-[75vh] bg-white">
-        <img src={ContactHero} alt="Studio" class="object-cover w-full h-[80vh]" />
+        <img src={ContactHeroImg} alt="Studio" class="object-cover w-full h-[80vh]" />
       </div>
     </div>
   );
